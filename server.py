9d34b763b8d29c6ed78aa475be7be95791165094
Your Name <yourgmail address>
2023-02-14 13:36:27 +0300

    update contact form

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import requests
 import datetime
+import smtplib
 
+my_email = 'vova@infomxxwedspb.ru'
+my_password = 'xffwqizdxsyuxwcxcxnr'
 
 app = Flask(__name__)
 
@@ -19,14 +22,31 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        msg = data["message"]
+        connection = smtplib.SMTP('smtp.yandex.com')
+        connection.starttls()
+        connection.login(my_email, my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="krahs123@gmail.com",
+            msg=f'Имя: {name}\n Почта: {email}\n Телефон: {phone}\n Сообщение: {msg}'.encode('utf-8').strip())
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
 
 
 @app.route('/post/<num>')
 def get_page(num):
     response = requests.get('https://jsonplaceholder.typicode.com/posts').json()
     return render_template("post.html", post=response, num=int(num))
+
+
 
 app.run(debug=True)
